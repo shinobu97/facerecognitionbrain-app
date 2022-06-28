54d3cb401d2404d0c49d3423b1335a2f0ad58e5c
shinobu97 <holidayworld100@gmail.com>
2022-06-28 18:23:01 -0400

    moving finished project files from pc to cloud

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,17 @@
+import React from "react";
+import brain from './brain.png';
+import './Logo.css';
+
+const Logo = () => {
+    return (
+        <div className='ma4 mt0'>
+            <div className="Tilt br2 shadow-2" style={{height: 150, width: 150}}>
+                <div className="pa3 center">
+                    <img style={{paddingTop: '5px'}} alt='logo' src={ brain }/>
+                </div> 
+            </div>
+        </div>
+    );
+}
+
+export default Logo;
